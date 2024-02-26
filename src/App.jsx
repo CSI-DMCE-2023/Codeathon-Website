4375737ac0fe50ad59093f0f034f9472bf267fb0
prathmesh <prathameshk990@gmail.com>
2024-02-27 02:01:55 +0530

    About us page Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 
 import './App.css'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        
+    
+      <div className='w-full h-full bg-slate-900 flex flex-col justify-start items-center gap-4 '>
+         <div class="cube-container">
+           <div class="cube">
+              <div class="face front"></div>
+              <div class="face back"></div>
+              <div class="face right"></div>
+              <div class="face left"></div>
+              <div class="face top"></div>
+              <div class="face bottom"></div>
+           </div>
+         </div>          
+        <About />
       </div>
      
-    </>
+    
   )
 }
 
