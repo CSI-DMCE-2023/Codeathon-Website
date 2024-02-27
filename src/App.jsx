b8145874b5e8006b871478cf391b654d0d23c514
Varad Dhumale <varaddhumale177@gmail.com>
2024-02-27 13:25:50 +0530

    unwanted code deleted

--- src/App.jsx
+++ src/App.jsx
@@ -26,16 +26,7 @@ function App() {
        <Home />
 
     <div className='w-full h-full bg-slate-900 flex flex-col justify-start items-center gap-4 '>
-         <div class="cube-container">
-           <div class="cube">
-              <div class="face front"></div>
-              <div class="face back"></div>
-              <div class="face right"></div>
-              <div class="face left"></div>
-              <div class="face top"></div>
-              <div class="face bottom"></div>
-           </div>
-         </div>          
+                 
         <About />
       </div> 
 
