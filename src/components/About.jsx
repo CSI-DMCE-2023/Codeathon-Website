6c98d8a8a27d5903080424b09a754403e28ada45
prathmesh <prathameshk990@gmail.com>
2024-02-27 22:42:31 +0530

    About upadte2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,8 @@ import AboutDetail from "./AboutDetail"
 import React from 'react'
 
 const About = () => {
-    return (
-        
-           
-        <div className="w-full  bg-slate-950 flex flex-col justify-start items-center gap-4">
+    return (  
+        <div className="w-full  bg-slate-900 flex flex-col justify-start items-center gap-4">
              <Cuboid/>
             <AboutDetail/>
             <Domain/>
