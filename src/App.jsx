2d7537982bb359bc8b564d9b9e77195b820caab8
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-02-29 19:06:56 +0530

    duplicate component removed

--- src/App.jsx
+++ src/App.jsx
@@ -83,10 +83,6 @@ function App() {
           <div className="w-full h-full bg-slate-900 flex flex-col justify-start items-center gap-4 ">
             <About />
           </div>
-          <div className="w-full h-full bg-slate-900 flex flex-col justify-start items-center gap-4 ">
-            <About />
-          </div>
-
           <TimeLine />
           <Gallery />
           <Sponcers />
