6d6f8aa74ed7055c12a0936c169143208b022c69
Varad Dhumale <varaddhumale177@gmail.com>
2024-03-02 00:13:47 +0530

    domain edited

--- src/components/Domain.jsx
+++ src/components/Domain.jsx
@@ -2,48 +2,47 @@ import "./Domain.css"
 
 function Domain() {
 
+
+  const objects = [
+    {
+      title: "SMART AUTOMATION",
+      imgLink: "https://img.freepik.com/free-photo/business-hand-robot-handshake-artificial-intelligence-digital-transformation_53876-138972.jpg?t=st=1709318529~exp=1709322129~hmac=4570f59360d09744b89f8dedf9f6322f9fb89e316405655279f733a9aabd5468&w=996"
+    },
+    {
+      title: "SOCIAL IMPACT",
+      imgLink: "https://img.freepik.com/free-photo/dictatorship-oppression-collage-concept_23-2149711433.jpg?t=st=1709318206~exp=1709321806~hmac=04c5c38e409b5d3608d9b09c47a1353d355579918eeac71f82cf88e6f4b5778c&w=996"
+    },
+    {
+      title: "FINTECH",
+      imgLink: "https://img.freepik.com/free-photo/fintech-investment-financial-internet-technology-concept_53876-124767.jpg?t=st=1709318565~exp=1709322165~hmac=4ffa730a9d27ad25b928e1ed2ba5702d83a2bece61301c772361a019cbf61352&w=996"
+    },
+    {
+      title: "SUSTAINABLE DEVELOPMENT",
+      imgLink: "https://img.freepik.com/free-photo/sustainable-development-goals-still-life_23-2150196699.jpg?t=st=1709318475~exp=1709322075~hmac=84b7d371023de8808625e6757a65a22f1e25a0cb399436f512ea13ba637a2e28&w=996"
+    }
+  ];
+  
+
   return (
+
+
+
     <>
-    
-      <p id="domains" className="text-sky-400 text-5xl font-extrabold mt-10">Domains</p>
-      <div class="container">
-        <div class="card">
-          <div class="Box">
-          <h2>SMART AUTOMATION</h2>
-            {/* <img src="https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> */}
-          </div>
-          <div class="details">
-            <h2>SMART AUTOMATION</h2>
-            {/* <p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p> */}
-          </div>
-        </div>
-        <div class="card">
-          <div class="Box">
-            {/* <img src="https://images.pexels.com/photos/3699319/pexels-photo-3699319.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> */}
-          </div>
-          <div class="details">
-            <h2>SOCIAL IMPACT</h2>
-            {/* <p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p> */}
-          </div>
-        </div>
-        <div class="card">
-          <div class="Box">
-            {/* <img src="https://images.pexels.com/photos/598917/pexels-photo-598917.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> */}
-          </div>
-          <div class="details">
-            <h2>FINTECH</h2>
-            {/* <p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p> */}
-          </div>
-        </div>
-        <div class="card">
-          <div class="Box">
-            {/* <img src="https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> */}
-          </div>
-          <div class="details">
-            <h2>SUSUTAIABLE DEVELOPMENT</h2>
-            {/* <p>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p> */}
-          </div>
-        </div>
+      <div class="container grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {
+          objects.map((item, i) => {
+            return <div class="card w-full md:w-[80%] mx-auto h-40 hover:shadow-2xl hover:shadow-sky-400 shadow-sm duration-150 shadow-sky-400 transition-shadow">
+              <div class="Box py-12">
+                <h2 className="text-white text-center font-bold text-2xl" >{item.title}</h2>
+
+              </div>
+              <div class="absolute inset-0 object-fit">
+                <img className="w-full h-full" src={item.imgLink} alt="loading"  />
+
+              </div>
+            </div>
+          })
+        }
       </div>
 
 
--- src/components/MainDomain.jsx
+++ src/components/MainDomain.jsx
@@ -4,7 +4,7 @@ import Domain from "./Domain";
 function MainDomain() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-auto  bg-black">
-      <p className=" h-[30%] p-10  md:text-7xl sm:text-4xl text-2xl  text-[white] font-bold  text-center  ">
+      <p className=" h-[30%] py-8  md:text-7xl sm:text-4xl text-2xl  text-[white] font-bold  text-center  ">
         DOMAINS
       </p>
       {/* <DomainCard /> */}
