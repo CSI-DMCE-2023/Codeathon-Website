767192433052021f770fe4ba20208f04cb51a2de
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-03-17 17:40:57 +0530

    home section bg

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,8 +7,9 @@ const Spline = React.lazy(() => import("@splinetool/react-spline"));
 export function Home({ setLogoLoader }) {
   return (
     <div id="vanta2" className="h-screen bg-black">
-      <div className="h-screen w-full flex items-center justify-center  overflow-hidden flex-col vanta overflow-x-hidden">
+      <div className="h-screen w-full flex items-center justify-center  overflow-hidden flex-col vanta overflow-x-hidden dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative ">
         {/* <StarsCanvas /> */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="flex-col w-full h-full z-50">
           <Spline
             className=" scale-50 md:scale-150 md:w-full w-[190%] -m-[50%] mt-20 sm:ml-0 bg-slate-50"
@@ -17,10 +18,10 @@ export function Home({ setLogoLoader }) {
               width: "revert-layer",
               background: "transparent",
             }}
-            scene="https://prod.spline.design/eatze73xxFkdvtg8/scene.splinecode"
+            scene="https://prod.spline.design/spV0nxuwKVr2DtBf/scene.splinecode"
             onLoad={() => setLogoLoader(false)}
           />
-          <div className="w-full flex justify-center items-center absolute bottom-[8%]">
+          <div className="w-full flex justify-center items-center absolute bottom-[20%] sm:bottom-[20%]">
             <button className="btn">Register Now</button>
           </div>
         </div>
--- src/components/domain/Domaincards.jsx
+++ src/components/domain/Domaincards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Domaincards({ imglogo, title, paragraph, index }) {
   return (
-    <div className="card hover:scale-105 rounded-xl flex-1 max-w-sm bg-white ease-in-out duration-500">
+    <div className="card hover:scale-105 rounded-xl flex-1  max-w-sm bg-white ease-in-out duration-500">
       <div className="px-3 pt-3 h-1/3">
         <img src={imglogo} className="rounded-xl h-full w-full object-cover" />
       </div>
