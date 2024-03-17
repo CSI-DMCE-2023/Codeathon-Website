0c818fd009406de91e3430b6372f7bbd9d9b6672
FaizanPotrick <faizan.potrick02@gmail.com>
2024-03-17 08:10:01 +0530

    Update footer page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./Navbar/Navbar";
 
 import { TimeLine } from "./TimeLine/TimeLine";
 import { Gallery } from "./Gallery/Gallery";
-import Footer from "./footer/Footer";
+import Footer from "./components/Footer";
 import { useEffect, useState } from "react";
 import PreLoader from "./preloader/PreLoader.jsx";
 import Contact from "./components/Contact";
@@ -18,7 +18,7 @@ import { motion, useScroll, useSpring } from "framer-motion";
 import Winner from "./Winner/Winner.jsx";
 import MainDomain from "./components/MainDomain.jsx";
 import Faq from "./FAQ/Faq.jsx";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import Marketing from "./marketing/Marketing.jsx";
 import Domains from "./domain/Domain.jsx";
 import ReactGA from "react-ga4";
@@ -34,10 +34,8 @@ function App() {
   ReactGA.initialize("G-E0D9FS4JDP");
   ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
-
   useEffect(() => {
-
-    const lenis = new Lenis()
+    const lenis = new Lenis();
 
     function raf(time) {
       lenis.raf(time);
@@ -56,11 +54,9 @@ function App() {
 
   return (
     <div className="">
-
       {loading ? (
         <PreLoader />
       ) : (
-
         <>
           <motion.div
             className="progress-bar"
@@ -79,32 +75,26 @@ function App() {
           />
           <Navbar />
 
-
           <Home />
-       
+
           {/* <div className="w-full h-full bg-slate-900 flex flex-col justify-start items-center gap-4 ">
             <About />
           </div> */}
-          <Domains/>
+          <Domains />
 
-         
-          
+          <TimeLine />
+          <Winner />
+          <Marketing />
+          <Gallery />
 
-
-            <TimeLine />
-            <Winner/>
-            <Marketing/>
-            <Gallery />
-
-            {/* <Sponcers /> */}
-            <Faq/>
-            <GoToTopBtn />
-            <Contact/>
-            <Footer />
-            <Analytics/>
-           
-          </>
-          )}
+          {/* <Sponcers /> */}
+          <Faq />
+          <GoToTopBtn />
+          <Contact />
+          <Footer />
+          <Analytics />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../varients";
+import LinkedIn from "../assets/icons/linkedin.png";
+import Instagram from "../assets/icons/instagram.png";
+import Discord from "../assets/icons/discord.png";
+
+const Footer = () => {
+  return (
+    <div className="bg-black text-white px-2 mt-8 sm:mt-12">
+      <motion.div
+        variants={fadeIn("up", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.7 }}
+        className="gap-y-4 py-4 flex flex-col justify-center items-center"
+      >
+        <div className="flex gap-x-6">
+          <a href="/" target="_blank">
+            <img src={LinkedIn} alt="linkedin" className="size-8 sm:size-9" />
+          </a>
+          <a href="/" target="_blank">
+            <img src={Instagram} alt="instagram" className="size-8 sm:size-9" />
+          </a>
+          <a href="/" target="_blank">
+            <img src={Discord} alt="discord" className="size-8 sm:size-9" />
+          </a>
+        </div>
+        <p className="text-xs sm:text-sm font-light">
+          DMCE 2024 | All Rights Are Reserved
+        </p>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Footer;
