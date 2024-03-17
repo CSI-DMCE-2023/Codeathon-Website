e43d8ea533022287213acc0c8a3633ae4d34ab40
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-03-17 18:22:42 +0530

    bg fix

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,9 +7,9 @@ const Spline = React.lazy(() => import("@splinetool/react-spline"));
 export function Home({ setLogoLoader }) {
   return (
     <div id="vanta2" className="h-screen bg-black">
-      <div className="h-screen w-full flex items-center justify-center  overflow-hidden flex-col vanta overflow-x-hidden dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative ">
+      <div className="h-screen w-full flex items-center justify-center  overflow-hidden flex-col vanta overflow-x-hidden bg-black bg-grid-white/[0.2] relative ">
         {/* <StarsCanvas /> */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <div className="flex-col w-full h-full z-50">
           <Spline
             className=" scale-50 md:scale-150 md:w-full w-[190%] -m-[50%] mt-20 sm:ml-0 bg-slate-50"
