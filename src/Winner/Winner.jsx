a1ecd35971871d36b6145764fae6a6054a5f154c
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-03-18 17:51:37 +0530

    winners

--- src/Winner/Winner.jsx
+++ src/Winner/Winner.jsx
@@ -43,11 +43,11 @@ const Winner = () => {
           });
         });
   
-        function formatNumber(value, decimals) {
-          let s = (+value).toLocaleString("en-US").split(".");
-          return decimals
-            ? s[0] + "." + ((s[1] || "") + "00000000").substr(0, decimals)
-            : s[0];
+        function formatNumber(num, decimals) {
+          return num.toLocaleString("en-IN", {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals,
+          });
         }
       }
     }
