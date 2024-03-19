cf05ee2c8f701da27401edea0253ba0913d1e0c5
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-03-19 11:56:34 +0530

    post about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import logithon from "../assets/LOGITHON.png"
 const About = () => {
   return (
     <section >
@@ -9,7 +9,7 @@ const About = () => {
             <p class="mb-4">Datta Meghe College of Engineering, in collaboration with Softlink Global Pvt Ltd, hosts LogiTHON, a pioneering 24-hour hackathon merging technology with logistics. Participants from all backgrounds converge to devise innovative solutions for logistics challenges. With a focus on efficiency, sustainability, and social impact and mentorship. It reshapes the future of logistics through supply chain optimization and last-mile delivery revolution, fostering an ecosystem where ideas flourish and partnerships thrive.</p>
         </div>
         <div class="grid grid-cols-2 gap-4 mt-8">
-            <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
+            <img class="w-full rounded-lg" src={logithon} alt="office content 1" />
             <img class="mt-4 w-full lg:mt-10 rounded-lg" src="https://plus.unsplash.com/premium_photo-1690303193793-498c692e9b5c?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="office content 2" />
         </div>
     </div>
