39e10e9a93946e520c6827b9b8018e4a80be50d6
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-03-19 18:12:49 +0530

    sponcers

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
       <CalltoAction />
       <Gallery />
       {/* <Sponcers /> */}
-      {/* <Sponcers /> */}
+      <Sponcers />
       <FAQs />
       <GoToTopBtn />
       <Contact />
