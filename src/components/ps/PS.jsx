b9b9cfe066c70a3f4ba7b30b0a5b5dd742bd9cf2
FaizanPotrick <faizan.potrick02@gmail.com>
2024-03-23 11:27:32 +0530

    Add video link in ps

--- src/components/ps/PS.jsx
+++ src/components/ps/PS.jsx
@@ -8,24 +8,32 @@ const PS = () => {
       imgLink: "/Intelligent_warehouse.jpg",
       paragraph:
         "Design an AI-driven system that optimizes warehouse space utilization, automates inventory management, and predicts stock levels based on historical data, future orders and current trends. The system should integrate and be compatible with devices for real-time tracking and provide actionable insights to reduce waste and improve operational efficiency.",
+      videoLink:
+        "https://drive.google.com/file/d/1umrlMxtnNo4I_jo4GaAV5rWuBHQ1GW75/view?usp=sharing",
     },
     {
       title: "International Supply Chain Data Sharing using Blockchain",
       imgLink: "/Supply_chain_data_sharing_using_blockchain.jpg",
       paragraph:
         "Create a blockchain-based platform that ensures end-to-end visibility and security in the international supply chain. This platform should allow all stakeholders, including suppliers, transporters, and customers, to access real-time, tamper-proof information about the product journey, from pickup to delivery, enhancing trust and compliance. Define Data Items that will be shared with various stakeholders. ",
+      videoLink:
+        "https://drive.google.com/file/d/1OFRO4vyWe_5LfRmYDh6vNpUXNkGEfPZ4/view?usp=sharing",
     },
     {
       title: "Dynamic Route Optimization for International Trade",
       imgLink: "/route_optimization.jpg",
       paragraph:
         "Create an algorithm or platform that optimizes international trade routes in real-time, taking into account various factors such as traffic conditions, weather, delivery windows, and vehicle capacity. This solution aims to reduce delivery times, fuel consumption, and operational costs.",
+      videoLink:
+        "https://drive.google.com/file/d/1oCLWrrGw8XGDJW3YhgdKRbUdx_uwT70p/view?usp=sharing",
     },
     {
       title: "A Self-Learning AI-Powered PDF to Data Converter",
       imgLink: "/pdf_data.png",
       paragraph:
         "Create a cutting-edge, self-learning Smart PDF to Data Converter that leverages Generative AI rather than relying on conventional OCR libraries. This system should utilize machine learning (ML) algorithms to continuously learn from its conversions and extract insights, enhancing its ability to accurately interpret and convert various PDF documents, regardless of their complexity or format.",
+      videoLink:
+        "https://drive.google.com/file/d/1iw6t-drSzHRC6wrkHA8JguY-MKnrFGPN/view?usp=sharing",
     },
   ];
 
@@ -46,6 +54,7 @@ const PS = () => {
                 paragraph={item.paragraph}
                 imglogo={item.imgLink}
                 index={index}
+                videoLink={item.videoLink}
                 key={index}
               />
             );
