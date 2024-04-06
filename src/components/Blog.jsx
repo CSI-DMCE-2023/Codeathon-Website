206f615d717e1a53fd81f9e1db5338446c443936
FaizanPotrick <faizan.potrick02@gmail.com>
2024-04-06 20:07:11 +0530

    update sponcers and blogs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,15 +3,12 @@ import axios from "axios";
 
 export default function Blog() {
   const [bloglist, setbloglist] = useState([]);
-  // const width = hasWindow ? window.innerWidth : null;
   const { innerWidth: width, innerHeight: height } = window;
-  console.log(width);
   useEffect(() => {
     const getblogdata = async () => {
       const { data } = await axios.get(
         "https://public-api.wordpress.com/rest/v1/sites/logithon.wordpress.com/posts"
       );
-      console.log(data.posts);
       setbloglist(data.posts);
     };
     getblogdata();
@@ -29,52 +26,52 @@ export default function Blog() {
             </h3>
           </div>
           {bloglist.length > 0 && (
-            <div class="grid gap-8 lg:grid-cols-3">
+            <div className="grid gap-8 lg:grid-cols-3">
               {bloglist.slice(0, 3).map((data, index) => (
-                <article class="p-6 bg-black rounded-lg border border-gray-200 shadow-md mx-3">
-                  <div class="flex justify-between items-center mb-5 text-gray-500">
-                    <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                <article key={index} className="p-6 bg-black rounded-lg border border-gray-200 shadow-md mx-3">
+                  <div className="flex justify-between items-center mb-5 text-gray-500">
+                    <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                       <svg
-                        class="mr-1 w-3 h-3"
+                        className="mr-1 w-3 h-3"
                         fill="currentColor"
                         viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         ></path>
                         <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
                       </svg>
                       LogiTHON
                     </span>
-                    <span class="text-sm">{data.date.split("T")[0]}</span>
+                    <span className="text-sm">{data.date.split("T")[0]}</span>
                   </div>
-                  <h2 class="mb-2 text-2xl font-bold tracking-tight text-white ">
+                  <h2 className="mb-2 text-2xl font-bold tracking-tight text-white ">
                     <a href={data.URL}>{data.title}</a>
                   </h2>
                   <div
                     dangerouslySetInnerHTML={{ __html: data.excerpt }}
-                    class="mb-5 font-light text-white "
+                    className="mb-5 font-light text-white "
                   ></div>
-                  <div class="flex justify-between items-center">
+                  <div className="flex justify-between items-center">
                     <a
                       href={data.URL}
-                      class="inline-flex items-center font-medium text-white hover:underline"
+                      className="inline-flex items-center font-medium text-white hover:underline"
                       target="_blank"
                     >
                       Read more
                       <svg
-                        class="ml-2 w-4 h-4"
+                        className="ml-2 w-4 h-4"
                         fill="currentColor"
                         viewBox="0 0 20 20"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fillRule="evenodd"
                           d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                          clip-rule="evenodd"
+                          clipRule="evenodd"
                         ></path>
                       </svg>
                     </a>
--- src/components/Sponcers.jsx
+++ src/components/Sponcers.jsx
@@ -44,9 +44,9 @@ function Sponcers() {
     <>
       <div className="max-w-7xl mx-auto py-12 px-4 text-white">
         <p className="w-full uppercase text-2xl md:text-4xl  font-semibold text-center">
-          Associate Partner
+          Associate Partners
         </p>
-        <div className="flex justify-center w-full items-center mt-12 gap-x-8 md:gap-x16">
+        <div className="flex flex-wrap justify-center w-full items-center mt-12 gap-8 md:gap-16">
           {data.map((item, index) => (
             <a key={index} href={item.link} target="_blank" title={item.name}>
               <img
