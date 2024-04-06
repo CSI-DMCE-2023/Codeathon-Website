5dd8f8c38e403c768b6bde4f6b54369aff3964cb
FaizanPotrick <faizan.potrick02@gmail.com>
2024-04-06 20:18:18 +0530

    update tpo page

--- src/TPOHRMeet.jsx
+++ src/TPOHRMeet.jsx
@@ -10,8 +10,8 @@ export default function TPOHRMeet() {
   // };
   return (
     <div>
-      <section className="min-h-screen pattern flex flex-col justify-between">
-        <nav className="py-4 md:pt-8 px-4 mx-auto">
+      <section className="px-5 pt-5 md:pt-12 min-h-screen pattern flex flex-col justify-between gap-y-10">
+        <nav className="mx-auto">
           <div className="flex items-center justify-center gap-x-3">
             <a href="https://www.dmce.ac.in/" target="_blank">
               <img
@@ -30,22 +30,22 @@ export default function TPOHRMeet() {
             </div>
           </div>
         </nav>
-        <div className="container px-5 pt-4 pb-6 md:py-12 mx-auto text-center flex flex-col justify-center">
+        <div className="mx-auto text-center flex flex-col justify-center">
           <div className="max-w-5xl mx-auto my-auto">
             <h1 className="text-3xl font-semibold text-white md:text-6xl">
               TPO - HR Meet
             </h1>
-            <h4 className="text-sm md:text-base my-2 text-gray-300 uppercase">
+            <h4 className="text-sm md:text-base my-2 text-gray-300 uppercase tracking-widest">
               Organized by
             </h4>
-            <h1 className="text-2xl font-semibold text-[#22D3EE] md:text-5xl">
+            <h1 className="text-2xl font-medium text-[#22D3EE] md:text-5xl tracking-widest uppercase">
               Mr. Faisal A. Khan
             </h1>
-            <h4 className="md:text-lg mt-1 text-gray-300 uppercase">
+            <h4 className="text-sm md:text-base mt-1 text-gray-300 uppercase tracking-widest">
               Director <br /> Training and Placement Cell
             </h4>
-            <p className="mt-6 md:mt-10 text-gray-300 md:text-lg max-w-3xl mx-auto text-justify">
-              Join us for our TPO & HR Meet, held in conjunction with LogiTHON,
+            <p className="mt-6 md:mt-10 text-gray-300 text-sm md:text-lg max-w-3xl mx-auto text-justify">
+              Join us for our TPO - HR Meet, held in conjunction with LogiTHON,
               to network with TPOs and HRs, and delve into the evolving
               landscape of tech-driven logistics solutions. LogiTHON is a
               leading national hackathon blending technology with logistics. It
@@ -61,8 +61,8 @@ export default function TPOHRMeet() {
               href="https://forms.gle/R5RMf2DagcZFUYFE8"
               target="_blank"
             >
-              <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-[#22D3EE] transition-transform group-hover:translate-x-0 group-hover:translate-y-0  rounded"></span>
-              <span className="relative inline-block border-2 border-[#22D3EE] px-8 py-3 text-sm font-bold uppercase tracking-widest text-white group-active:text-opacity-75">
+              <span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-[#22D3EE] transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
+              <span className="relative inline-block border-2 border-white px-6 md:px-8 py-2 md:py-3 text-sm font-bold uppercase tracking-widest text-white group-hover:border-[#22D3EE] group-active:text-opacity-75">
                 Join Us
               </span>
             </a>
