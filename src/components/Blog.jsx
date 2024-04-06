1e0084426efb92bf3ff212243c2e4e16751a741a
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-04-06 11:27:35 +0530

    blogs cards

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Slider from "infinite-react-carousel";
 
 export default function Blog() {
+  const [bloglist, setbloglist] = useState([]);
+  // const width = hasWindow ? window.innerWidth : null;
+  const { innerWidth: width, innerHeight: height } = window;
+  console.log(width)
+  useEffect(() => {
+    const getblogdata = async () => {
+      const { data } = await axios.get(
+        "https://public-api.wordpress.com/rest/v1/sites/logithon.wordpress.com/posts"
+      );
+      console.log(data.posts);
+      setbloglist(data.posts);
+    };
+    getblogdata();
+  }, []);
   return (
     <div>
       <section>
@@ -9,107 +25,78 @@ export default function Blog() {
             <h1 className="py-6 sm:py-8 text-3xl md:text-5xl text-white font-semibold text-center mt-6 sm:mt-8">
               Our Blogs
             </h1>
-            <h3 className=" text-white font-bold sm:text-2xl text-lg">Want some insider Info ? Be sure to check out this blogs. we upload Daily!</h3>
+            <h3 className=" text-white font-bold sm:text-2xl text-lg">
+              Want some insider Info ? Be sure to check out this blogs. we
+              upload Daily!
+            </h3>
           </div>
-          <div class="grid gap-8 lg:grid-cols-2">
-            <article class="p-6 bg-black rounded-lg border border-gray-200 shadow-md ">
-              <div class="flex justify-between items-center mb-5 text-gray-500">
-              <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                  <svg
-                    class="mr-1 w-3 h-3"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                      clip-rule="evenodd"
-                    ></path>
-                    <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-                  </svg>
-                  Article
-                </span>
-                <span class="text-sm">2 days ago</span>
-              </div>
-              <h2 class="mb-2 text-2xl font-bold tracking-tight text-white ">
-                <a href="https://medium.com/@csi.dmce/unveiling-logithon-2024-where-innovation-meets-logistics-c7e531a6d6da">Unveiling LogiTHON 2024: Where Innovation Meets Logistics</a>
-              </h2>
-              <p class="mb-5 font-light text-white">
-              Excitement is building as the countdown begins for LogiTHON 2024, the groundbreaking national-level hackathon poised to redefine the intersection of technology and logistics.
-              </p>
-              <div class="flex justify-between items-center">
-                <a
-                  href="https://medium.com/@csi.dmce/unveiling-logithon-2024-where-innovation-meets-logistics-c7e531a6d6da"
-                  class="inline-flex items-center font-medium text-white hover:underline"
-                  target="_blank"
-                >
-                  Read more
-                  <svg
-                    class="ml-2 w-4 h-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </a>
-              </div>
-            </article>
-            <article class="p-6 bg-black rounded-lg border border-gray-200 shadow-md">
-              <div class="flex justify-between items-center mb-5 text-gray-500">
-                <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                  <svg
-                    class="mr-1 w-3 h-3"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
-                      clip-rule="evenodd"
-                    ></path>
-                    <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
-                  </svg>
-                  Article
-                </span>
-                <span class="text-sm">Apr 3, 2024</span>
-              </div>
-              <h2 class="mb-2 text-2xl font-bold tracking-tight text-white ">
-                <a href="https://logithon.wordpress.com/2024/04/03/sealed-amp-loaded-logithon-preps-shift-into-high-gear/">LogiTHON: India's BIGGEST Logistics Hackathon</a>
-              </h2>
-              <p class="mb-5 font-light text-white">
-              We've officially hit the brakes on registrations, and let me tell you, the response has been off the charts! With over 1,400 registrations and 545 teams signed up from across 236 colleges, it's safe to say we've got some serious brain power gearing up for this ride.
-              </p>
-              <div class="flex justify-between items-center">
-
-                <a
-                  href="https://logithon.wordpress.com/2024/04/03/sealed-amp-loaded-logithon-preps-shift-into-high-gear/"
-                  class="inline-flex items-center font-medium text-white  hover:underline"
-                  target="_blank"
-                >
-                  Read more
-                  <svg
-                    class="ml-2 w-4 h-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </a>
-              </div>
-            </article>
-          </div>
+          {bloglist.length>0 && (
+            <Slider
+            dots
+            autoplay={true}
+            className=" w-full "
+            autoplayScroll={3}
+            autoplaySpeed={10000}
+            arrows={false}
+            slidesPerRow={width>600?2:1}
+            adaptiveHeight={true}
+          >
+            { bloglist.map((data) => (
+                <div className="">
+                  <article class="p-6 bg-black rounded-lg border border-gray-200 shadow-md mx-3">
+                    <div class="flex justify-between items-center mb-5 text-gray-500">
+                      <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                        <svg
+                          class="mr-1 w-3 h-3"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                            clip-rule="evenodd"
+                          ></path>
+                          <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path>
+                        </svg>
+                        Article
+                      </span>
+                      <span class="text-sm">{data.date.split("T")[0]}</span>
+                    </div>
+                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-white ">
+                      <a href={data.URL}>{data.title}</a>
+                    </h2>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: data.excerpt }}
+                      class="mb-5 font-light text-white "
+                    ></div>
+                    <div class="flex justify-between items-center">
+                      <a
+                        href={data.URL}
+                        class="inline-flex items-center font-medium text-white hover:underline"
+                        target="_blank"
+                      >
+                        Read more
+                        <svg
+                          class="ml-2 w-4 h-4"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                          ></path>
+                        </svg>
+                      </a>
+                    </div>
+                  </article>
+                </div>
+              ))}
+          </Slider>
+          )}
+          
         </div>
       </section>
     </div>
