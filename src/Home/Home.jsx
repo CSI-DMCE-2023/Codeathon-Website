d1961c71d9a1e48374f4b42dc92f433286304794
Karan2310 <karandeep9619@gmail.com>
2024-04-06 20:59:13 +0530

    button resized

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -27,8 +27,8 @@ export function Home({ setLogoLoader }) {
             scene="https://prod.spline.design/spV0nxuwKVr2DtBf/scene.splinecode"
             onLoad={() => setLogoLoader(false)}
           />
-          <a onClick={() => scrollToTimeline("shortlisted-teams")}className=" flex justify-center items-center absolute sm:left-[43%] left-[15%]  bottom-[20%] sm:bottom-[15%]">
-            <button className="btn">Round 1 Results are live </button>
+          <a onClick={() => scrollToTimeline("shortlisted-teams")} className=" flex justify-center items-center absolute sm:left-[43%] left-[15%]  bottom-[20%] sm:bottom-[15%]">
+            <button className="btn w-auto">Round 1 Results are live </button>
           </a>
         </div>
       </div>
