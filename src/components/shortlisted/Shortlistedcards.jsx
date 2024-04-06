53bf92c5009600ec95538e6d3ff9bc022b486daa
FaizanPotrick <faizan.potrick02@gmail.com>
2024-04-06 10:52:14 +0530

    add shortlist teams name

--- src/components/shortlisted/Shortlistedcards.jsx
+++ src/components/shortlisted/Shortlistedcards.jsx
@@ -3,9 +3,9 @@ import { CardContainer, CardItem } from "../../ui/3d-card";
 
 function Shortlistedcards({ teamname }) {
   return (
-    <CardContainer className="w-48 h-16 rounded-md border hover:shadow-2xl hover:shadow-gray-500/[0.1] bg-black border-white/[0.4]">
+    <CardContainer className="w-48 h-16 rounded-md border hover:shadow-2xl hover:shadow-gray-500/[0.1] bg-black border-white/[0.2]">
       <CardItem translateZ="100">
-        <h3 className="text-white font-bold">{teamname}</h3>
+        <h3 className="text-white font-bold text-center px-2">{teamname}</h3>
       </CardItem>
     </CardContainer>
   );
